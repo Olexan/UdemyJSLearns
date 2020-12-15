e066839cccf561b524e0802499c73621c2f49dae
Olexan <olexan@gmail.com>
2020-12-15 17:33:54 +0200

    start

---
+++ js/script.js
@@ -0,0 +1,10 @@
+
+"use strict";
+
+const number = 25;
+const Name ='Olexan';
+
+//alert (`Hello ${Name}`);
+//console.log(`You number - ${number}`);  Интерполяция строк
+
+console.log(4 + "9");
---
+++ js/start.js
@@ -0,0 +1,2 @@
+var name = "User";
+alert (name);
