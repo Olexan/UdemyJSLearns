8e11cb9e723a51bda220998556b936067848d943
Olexan <olexan@gmail.com>
2020-12-16 20:03:31 +0200

    Практическая работа , задание 1

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
-
+/*
 "use strict";
-
 const number = 25;
 const Name ='Olexan';
-
-//alert (`Hello ${Name}`);
-//console.log(`You number - ${number}`);  Интерполяция строк
-
+alert (`Hello ${Name}`);
+console.log(`You number - ${number}`);  Интерполяция строк
 console.log(4 + "9");
+*/
+const numberOfFilms = +prompt('Сколько фильмов Вы посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+    const a = prompt('Один из последних просмотреных фильмов?', ''),
+          b = prompt('На сколько оцените его?', ''),
+          c = prompt('Один из последних просмотреных фильмов?', ''),
+          d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
