345df9a6c733669e06ecf4bbb8eefccd41c0da38
Olexan <olexan@gmail.com>
2020-12-16 20:44:20 +0200

    clean code

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
 /*
 "use strict";
 const number = 25;
-const Name ='Olexan';
-alert (`Hello ${Name}`);
 console.log(`You number - ${number}`);  Интерполяция строк
-console.log(4 + "9");
 */
 const numberOfFilms = +prompt('Сколько фильмов Вы посмотрели?', '');
 
